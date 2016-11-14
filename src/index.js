2d534806dadc5b7525799f9598f4ddefc3ddf57c
kjirou <sorenariblog@gmail.com>
2016-11-14 20:05:25 +0900

    Define sizes

--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ const Root = require('./components/Root');
 
 
 window.document.addEventListener('DOMContentLoaded', () => {
-  const container = window.document.querySelector('.js-app-container');
+  const container = window.document.querySelector('.js-dap-container');
   ReactDOM.render(React.createElement(Root), container);
 });
